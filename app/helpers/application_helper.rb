9bf6dbbf702eaf9986f51dec0b73a87470281786
JetStarBlues <JetStarBlues@users.noreply.github.com>
2013-07-17 15:45:13 -0400

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Lesson 22 @ 5:00 - something to do with title page
+	# Return a title on a per-page basis
+	def title
+		base_title = "RoR Sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
