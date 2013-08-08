50729510cf8f1834467162a514783497ea774152
JetStarBlues <JetStarBlues@users.noreply.github.com>
2013-08-08 18:51:59 -0400

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+	attr_accessible :name, :email
+
+    #check if name & email present
+    	#Lesson 36
+    	email_regex = /\A[\w\-.]+@[a-z\d\_]{2,}+\.[a-z]{2,}\z/i
+
+	    validates :name,  :presence => true,
+	    				  :length   => { :maximum => 50 }
+	    validates :email, :presence    => true,
+	    				  :format      => { :with => email_regex },
+	    				  #:uniqueness => true
+	    				  :uniqueness  => { :case_sensitive => false } 
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,61 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+  	@var = {:name => "exampleUser", :email => "user@example.com"}
+  end
+
+  #Lesson 36 @7:00
+  it "should create a new instance given a valid attribute" do
+  	User.create!(@var)
+  end
+
+  it "should require a name" do
+  	#replace 'exampleUser' with blank
+  	noNameUser = User.new(@var.merge(:name => ""))
+  	noNameUser.should_not be_valid
+  end
+
+  it "should require an email address" do
+  	noNameUser = User.new(@var.merge(:email => ""))
+  	noNameUser.should_not be_valid
+  end
+
+  it "should reject names that are too long" do
+  	longName = "a" * 51
+  	longNameUser = User.new(@var.merge(:name => longName))
+  	longNameUser.should_not be_valid
+  end
+
+ #by no means robust...(domains,length,@symbol ...see user.rb 'email_regex')
+  it "should accept valid email address" do
+  	addr = %w[diamond@casino.com spades@casino.org queen.hearts@casino.uk queen-hearts@casino.uk spades@2casino.org spades@2_casino.org]
+  	addr.each do |addr|
+  		validEmailUser = User.new(@var.merge(:email => addr))
+  		validEmailUser.should be_valid
+  	end
+  end
+ #by no means robust...(typos...abc@aol.orgg)
+  it "should reject invalid email address" do
+  	addr = %w[diamond@casino,com spades@a.com]
+  	addr.each do |addr|
+  		invalidEmailUser = User.new(@var.merge(:email => addr))
+  		invalidEmailUser.should_not be_valid
+  	end
+   end
+
+  it "should reject duplicate emails" do
+  	User.create!(@var)
+  	userWithDuplicateEmail = User.new(@var)
+  	userWithDuplicateEmail.should_not be_valid
+  end
+
+  it "should reject duplicate up to case" do
+  	upperCaseEmail = "USER@ExamplE.COM"
+  	User.create!(@var.merge(:email => upperCaseEmail))
+  	userWithDuplicateEmail = User.new(@var)
+  	userWithDuplicateEmail.should_not be_valid
+  end
+
 end
