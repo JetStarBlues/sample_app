4776288ce1c2f39e2d06823b484ab460b2150c00
JetStarBlues <JetStarBlues@users.noreply.github.com>
2013-08-10 14:54:58 -0400

    attempt3 to fix production Signup errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,10 @@ class UsersController < ApplicationController
       #handle a succesful save
       #flash[:success] = "Welcome USER, a great journey begins!"
       flash[:success] = "Welcome #{@user.name} - A great journey begins!"
-      redirect_to @user #, :flash => { :success => "Welcome USER, a great journey begins!" }
+      #redirect_to @user
+      redirect_to user_path(@user)
       # same as, redirect_to user_path(@user)
+      #redirect_to @user, :flash => { :success => "Welcome USER, a great journey begins!" }
     else
       @title = "Sign Up"
   	  render 'new'
