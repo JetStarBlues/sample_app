20bb30a1228cad6ab0363a3d7a0772da92ef388f
JetStarBlues <JetStarBlues@users.noreply.github.com>
2013-08-10 15:32:52 -0400

    attempt4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,7 @@ class UsersController < ApplicationController
       #handle a succesful save
       #flash[:success] = "Welcome USER, a great journey begins!"
       flash[:success] = "Welcome #{@user.name} - A great journey begins!"
-      #redirect_to @user
-      redirect_to user_path(@user.id) #JK to fix heroku
+      redirect_to @user
       # same as, redirect_to user_path(@user)
       #redirect_to @user, :flash => { :success => "Welcome USER, a great journey begins!" }
     else
